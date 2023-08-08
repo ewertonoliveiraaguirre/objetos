08bb13d9eb311205391960538e0eddcabcea3198
ewertonoliveiraaguirre <126290541+ewertonoliveiraaguirre@users.noreply.github.com>
2023-08-08 14:07:51 -0400

    Add files via upload

---
+++ main-fineshed.js
@@ -0,0 +1,11 @@
+ball.push(ball)
+
+function loop() {
+    ctx.fillStyle = 'rgba(0,0,0,0.25)'
+    ctx.fillRect (0,0,width,height)
+
+    for(const ball or balls ){
+        ball.draws()
+        ball.update()
+        ball.collission
+    }
